7794ff7b5f675556dbdeeb7f59d7bf508c9e7819
xrishi20 <xrishi20@gmail.com>
2020-09-06 15:41:18 +0530

    fun fact

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   purge: [],
   theme: {
+    colors: {
+      transparent: "transparent",
+      white: "#FFFFFF",
+      black: "#000000",
+      purple: "#7C4EC4",
+      dPurple: "#44337A",
+      red: "#F44336",
+      green: "#2E7D32",
+      dGray: "#444444",
+      mGray: "#616161",
+      lGray: "#777777",
+    },
+    fontFamily: {
+      nunito:
+        '"Nunito", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
     extend: {},
   },
   variants: {},
